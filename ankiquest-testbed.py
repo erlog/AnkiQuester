dfe86bca953c9bb1068fe70f22cd09ee3a008866
erlog <jjrozewicki@gmail.com>
2014-02-26 15:52:24 +0900

    player walking around with static NPC entity based on Roguelike Python tutorial

--- ankiquest-testbed.py
+++ ankiquest-testbed.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 
-
-
 import pdb
-import random
 import libtcodpy as libtcod
+import sys
 
 class WorldMechanics:
 	def __init__(self):
@@ -24,63 +22,63 @@ class Entity:
 							"Speed": 100,
 							"Luck": 10,
 							}
-	
+		self.X = 0
+		self.Y = 0
+		self.Tile = "@"
+		Entities.append(self)
+		
 	def TakeDamage(self, amount):
 		opponent.Stats["HP"] -= amount
 		
-def handle_keys():
-    global playerx, playery
+def handle_key(key):
+	if key.vk == libtcod.KEY_ENTER and key.lalt:
+		#Alt+Enter: toggle fullscreen
+		libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+	elif key.vk == libtcod.KEY_ESCAPE:
+		sys.exit()
+ 
+	#movement keys
+	if libtcod.console_is_key_pressed(libtcod.KEY_UP):
+		Player.Y -= 1
+	elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
+		Player.Y += 1
+	elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
+		Player.X -= 1
+	elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
+		Player.X += 1
 
-    key = libtcod.console_wait_for_keypress(True)  #turn-based
- 
-    if key.vk == libtcod.KEY_ENTER and key.lalt:
-        #Alt+Enter: toggle fullscreen
-        libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
- 
-    elif key.vk == libtcod.KEY_ESCAPE:
-        return True  #exit game
- 
-    #movement keys
-    if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-        playery -= 1
- 
-    elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-        playery += 1
- 
-    elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-        playerx -= 1
- 
-    elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-        playerx += 1
-
-if __name__ == "__main__":
-	#erlog = Entity()
-	#ckky = Entity()
-	#mechanics = WorldMe
-	#erlog.Attack(ckky)
-	#print ckky.Stats
+def draw_entities():
+	for entity in Entities:
+		libtcod.console_put_char(con, entity.X, entity.Y, entity.Tile, libtcod.BKGND_NONE)
 	
+def erase_entities():
+	for entity in Entities:
+		libtcod.console_put_char(con, entity.X, entity.Y, " ", libtcod.BKGND_NONE)
+	
+if __name__ == "__main__":
+	#Our game stuff
+	global Player, Entities, con
+	Entities = []
+	Player = Entity()
+	ckky = Entity()
+	ckky.X = 40
+	ckky.Y = 25
+	ckky.Tile = "d"
+	
+	#Set up libtcod
 	SCREEN_WIDTH = 80
 	SCREEN_HEIGHT = 50
 	LIMIT_FPS = 10
-	
 	libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 	libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python/libtcod tutorial', False)
 	libtcod.sys_set_fps(LIMIT_FPS)
-	 
-	playerx = SCREEN_WIDTH/2
-	playery = SCREEN_HEIGHT/2
- 
+	con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+	
 	while not libtcod.console_is_window_closed():
-	 
-		#libtcod.console_set_default_foreground(0, libtcod.white)
-		libtcod.console_put_char(0, playerx, playery, '@', libtcod.BKGND_NONE)
-	 
+		draw_entities()
+		libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
 		libtcod.console_flush()
+		erase_entities()
+		handle_key(libtcod.console_wait_for_keypress(True))
+		
 	 
-		libtcod.console_put_char(0, playerx, playery, ' ', libtcod.BKGND_NONE)
-	 
-		#handle keys and exit game if needed
-		exit = handle_keys()
-		if exit:
-			break
